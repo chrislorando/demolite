287047d32eeabb5e958d71e789dc0e4b8de5ea29
CML <chrismanuel.lorando@gmail.com>
2025-11-11 03:39:03 +0700

    feat(livewire): add DocumentVerifier Table component

---
+++ app/Livewire/DocumentVerifier/Table.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Livewire\DocumentVerifier;
+
+use App\Models\Document;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class Table extends Component
+{
+    use WithPagination;
+
+    public string $search = '';
+
+    protected $updatesQueryString = ['search'];
+
+    protected $listeners = [
+        'document-created' => '$refresh',
+        'document-deleted' => '$refresh',
+    ];
+
+    public function updatingSearch(): void
+    {
+        $this->resetPage();
+    }
+
+    public function view(string $id): void
+    {
+        $this->dispatch('view-document', documentId: $id);
+    }
+
+    public function confirmDelete(string $id): void
+    {
+        $this->dispatch('delete-document', documentId: $id);
+    }
+
+    public function render()
+    {
+        $documents = Document::query()
+            ->where('user_id', auth()->id())
+            ->when($this->search, function($q){
+                $q->whereLike('name',  '%'.$this->search.'%')
+                ->orWhereLike('instructions',  '%'.$this->search.'%')
+                ->orWhereLike('response',  '%'.$this->search.'%');
+            })
+            ->latest()
+            ->paginate(10);
+
+        return view('livewire.document-verifier.table', [
+            'documents' => $documents,
+        ]);
+    }
+}
