296dc3af57d95beb6b333285e3758fdae62ceea3
CML <chrismanuel.lorando@gmail.com>
2025-11-11 03:38:28 +0700

    chore(routes): update document verifier routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 use Livewire\Volt\Volt;
 use App\Livewire\Chat\SharedConversation as SharedConversationComponent;
+use OpenAI\Laravel\Facades\OpenAI;
 
 Route::get('/', function () {
     return view('welcome');
@@ -51,3 +53,40 @@ Route::middleware(['auth'])->group(function () {
 
 // Public route for shared conversations
 Route::get('/shared/{token}', SharedConversationComponent::class)->name('chat.shared.show');
+
+Route::get('documents', \App\Livewire\DocumentVerifier\Index::class)
+    ->middleware(['auth', 'verified'])
+    ->name('documents.index');
+
+Route::get('/pdf', function () {
+ $response = OpenAI::responses()->create([
+            'model' => 'gpt-4o-mini',
+            'input' => [
+                [
+                    'role' => 'user',
+                    'content' => [
+                        [
+                            'type' => 'input_text',
+                            'text' => "
+                                You are a document validator. This document can contain various types of forms or letters.
+                                Your task:
+                                1. Identify information or sections that appear to be mandatory in context (e.g., the applicant's name, signature, date, identification number, or sections that are mentioned but are blank).
+                                2. Determine whether the text is complete and consistent.
+                                3. Provide a summary of any missing or inconsistent information.
+                                3. Return the results in the following JSON format:
+                                [
+                                    {\"page\": <page number>, \"section\": \"<section or sentence>\", \"issue\": \"<problem>\", \"suggestion\": \"<what needs to be improved>\"}
+                                ]"
+                        
+                        ],
+                        [
+                            'type' => 'input_file',
+                            'file_url' => 'https://s3.demolite.my.id/demolite/employee_form_sample.pdf',
+                        ],
+                    ],
+                ],
+            ],
+        ]);
+
+        dd($response);
+});
