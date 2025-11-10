1500e9f239a192b1de92e021cb05f37705d0d0ac
CML <chrismanuel.lorando@gmail.com>
2025-11-11 03:40:11 +0700

    test(feature): add Documents feature test

---
+++ tests/Feature/DocumentsTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Models\User;
+use function Pest\Laravel\actingAs;
+
+it('shows documents page to authenticated user', function () {
+    $user = User::factory()->create();
+
+    actingAs($user)
+        ->get('/documents')
+        ->assertSuccessful()
+        ->assertSeeText('Documents');
+});
